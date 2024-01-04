49e2033a9fcb485d453e2e8bff1f08b6033e606d
Anntx9 <108412090+Anntx9@users.noreply.github.com>
2024-01-04 13:12:21 +0700

    Add files via upload

--- site.config.ts
+++ site.config.ts
@@ -2,23 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'dbb9c5a79c764693ab35cae12a83eecb',
+  rootNotionPageId: 'An-NT-dbb9c5a79c764693ab35cae12a83eecb',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Annt.io.vn',
+  name: 'An NT',
   domain: 'annt.io.vn',
   author: 'An NT',
 
   // open graph metadata (optional)
-  description: 'annt.io.vn',
+  description: 'Personal site of An Nguyen Thanh aka Thanh An',
 
   // social usernames (optional)
   twitter: 'annt186681',
-//   github: 'transitive-bullshit',
+//   github: 'annt',
   linkedin: 'annt96',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
