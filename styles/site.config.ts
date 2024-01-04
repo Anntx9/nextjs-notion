1f55f4883763d2cbb9c14deef8947a69a1baf813
Anntx9 <108412090+Anntx9@users.noreply.github.com>
2024-01-04 14:00:05 +0700

    Add files via upload

---
+++ styles/site.config.ts
@@ -0,0 +1,65 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'An-NT-dbb9c5a79c764693ab35cae12a83eecb',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'An NT',
+  domain: 'annt.io.vn',
+  author: 'An NT',
+
+  // open graph metadata (optional)
+  description: 'Personal site of An Nguyen Thanh aka Thanh An',
+
+  // social usernames (optional)
+  twitter: 'annt186681',
+//   github: 'annt',
+  linkedin: 'annt96',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+//   navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'About-e51c6121c32b4e7ead6544261d428b9d'
+    },
+    {
+      title: 'Contact',
+      pageId: 'Contact-18a89c6c52e84af2b4a8aa89780fc7d5'
+    }
+  ]
+})
